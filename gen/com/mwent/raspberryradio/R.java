89ff08aad34f3de463475d253bf07ed06d3d8ac0
Dunky13 <mwent@live.nl>
2013-06-29 17:43:27 +0200

    reload everything

--- gen/com/mwent/raspberryradio/R.java
+++ gen/com/mwent/raspberryradio/R.java
@@ -135,50 +135,54 @@ or to a theme attribute in the form "<code>?[<i>package</i>:][<i>type</i>:]<i>na
         public static final int slidingmenu_offset=0x7f050002;
     }
     public static final class drawable {
-        public static final int ic_action_add=0x7f020000;
-        public static final int ic_action_bookmark=0x7f020001;
-        public static final int ic_action_playback_next=0x7f020002;
-        public static final int ic_action_playback_play=0x7f020003;
-        public static final int ic_action_playback_prev=0x7f020004;
-        public static final int ic_action_playback_stop=0x7f020005;
-        public static final int ic_launcher=0x7f020006;
-        public static final int list=0x7f020007;
-        public static final int save=0x7f020008;
-        public static final int settings=0x7f020009;
-        public static final int shadow=0x7f02000a;
-        public static final int shadowright=0x7f02000b;
+        public static final int default_album_image=0x7f020000;
+        public static final int ic_action_add=0x7f020001;
+        public static final int ic_action_bookmark=0x7f020002;
+        public static final int ic_action_playback_next=0x7f020003;
+        public static final int ic_action_playback_play=0x7f020004;
+        public static final int ic_action_playback_prev=0x7f020005;
+        public static final int ic_action_playback_stop=0x7f020006;
+        public static final int ic_launcher=0x7f020007;
+        public static final int list=0x7f020008;
+        public static final int save=0x7f020009;
+        public static final int settings=0x7f02000a;
+        public static final int shadow=0x7f02000b;
+        public static final int shadowright=0x7f02000c;
     }
     public static final class id {
-        public static final int action_settings=0x7f04001b;
-        public static final int action_stations=0x7f04001c;
-        public static final int add_server=0x7f04000d;
-        public static final int content_frame=0x7f040013;
-        public static final int editText1=0x7f04000f;
-        public static final int editText2=0x7f040012;
+        public static final int action_settings=0x7f04001f;
+        public static final int action_stations=0x7f040020;
+        public static final int add_server=0x7f040010;
+        public static final int album_image=0x7f040006;
+        public static final int button_dividerHorizontal=0x7f040007;
+        public static final int button_dividerVertical=0x7f040018;
+        public static final int button_dividerVerticalMain1=0x7f040009;
+        public static final int button_dividerVerticalMain2=0x7f04000b;
+        public static final int button_dividerVerticalMain3=0x7f04000d;
         public static final int fullscreen=0x7f040003;
-        public static final int imageButton1=0x7f040008;
-        public static final int imageButton2=0x7f040009;
-        public static final int imageButton3=0x7f04000a;
-        public static final int imageButton4=0x7f04000b;
-        public static final int imageView1=0x7f040006;
         public static final int left=0x7f040000;
+        public static final int left_frame=0x7f04001a;
         public static final int margin=0x7f040002;
-        public static final int menu_frame=0x7f040015;
-        public static final int menu_frame_one=0x7f040016;
-        public static final int menu_frame_two=0x7f040017;
+        public static final int menu_frame=0x7f04001b;
+        public static final int next=0x7f04000e;
         public static final int pager=0x7f040005;
+        public static final int play=0x7f04000c;
+        public static final int prev=0x7f040008;
         public static final int right=0x7f040001;
-        public static final int row_icon=0x7f040018;
-        public static final int row_title=0x7f040019;
-        public static final int sattings_save=0x7f04001d;
-        public static final int section_label=0x7f040014;
+        public static final int right_frame=0x7f04001c;
+        public static final int row_title=0x7f04001d;
         public static final int selected_view=0x7f040004;
-        public static final int server=0x7f04000c;
-        public static final int slidingmenumain=0x7f04001a;
-        public static final int tableRow1=0x7f040007;
-        public static final int tableRow2=0x7f040010;
-        public static final int textView1=0x7f04000e;
-        public static final int textView2=0x7f040011;
+        public static final int server=0x7f04000f;
+        public static final int settings_cancel=0x7f040017;
+        public static final int settings_layout=0x7f040011;
+        public static final int settings_password=0x7f040014;
+        public static final int settings_save=0x7f040019;
+        public static final int settings_server_ip=0x7f040015;
+        public static final int settings_server_port=0x7f040016;
+        public static final int settings_servername=0x7f040012;
+        public static final int settings_username=0x7f040013;
+        public static final int slidingmenumain=0x7f04001e;
+        public static final int stop=0x7f04000a;
     }
     public static final class integer {
         public static final int num_cols=0x7f060000;
@@ -187,27 +191,37 @@ or to a theme attribute in the form "<code>?[<i>package</i>:][<i>type</i>:]<i>na
         public static final int activity_main=0x7f030000;
         public static final int activity_server=0x7f030001;
         public static final int activity_settings=0x7f030002;
-        public static final int content_frame=0x7f030003;
-        public static final int fragment_main_dummy=0x7f030004;
-        public static final int list=0x7f030005;
-        public static final int menu_frame=0x7f030006;
-        public static final int menu_frame_one=0x7f030007;
-        public static final int menu_frame_two=0x7f030008;
-        public static final int row=0x7f030009;
-        public static final int slidingmenumain=0x7f03000a;
+        public static final int left_frame=0x7f030003;
+        public static final int list=0x7f030004;
+        public static final int menu_frame=0x7f030005;
+        public static final int right_frame=0x7f030006;
+        public static final int row=0x7f030007;
+        public static final int slidingmenumain=0x7f030008;
     }
     public static final class menu {
         public static final int main=0x7f090000;
-        public static final int server=0x7f090001;
-        public static final int settings=0x7f090002;
     }
     public static final class string {
         public static final int action_settings=0x7f070001;
         public static final int action_station=0x7f070002;
         public static final int add_server=0x7f070003;
+        public static final int album_image=0x7f070006;
         public static final int app_name=0x7f070000;
+        public static final int cancel=0x7f070011;
         public static final int image_text=0x7f070004;
+        public static final int next=0x7f07000a;
+        public static final int password=0x7f07000e;
+        public static final int play=0x7f070009;
+        public static final int prev=0x7f070007;
+        public static final int save=0x7f070012;
+        public static final int server_ip=0x7f07000f;
+        public static final int server_name=0x7f07000c;
+        public static final int server_port=0x7f070010;
+        public static final int settings_title=0x7f07000b;
+        public static final int song_name=0x7f070013;
+        public static final int stop=0x7f070008;
         public static final int title_bar_slide=0x7f070005;
+        public static final int username=0x7f07000d;
     }
     public static final class style {
         /** 
